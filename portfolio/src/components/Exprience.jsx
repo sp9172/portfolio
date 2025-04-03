f769f19d41d3573cc8d6aead5f6d9dfb5d0e1111
Prathamesh Surywanshi <psurywanshi620@gmail.com>
2025-04-03 15:04:36 +0530

    exprience page working

--- portfolio/src/components/Exprience.jsx
+++ portfolio/src/components/Exprience.jsx
@@ -4,20 +4,26 @@ const Exprience = () => {
   return (
     <section>
             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
-            <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
+            <div className="grid grid-cols-1  gap-4 lg:grid-cols-3 lg:gap-8">
     <div className="">
     <h2 class="md:text-7xl font-bold text-black capitalize pb-8 underline decoration-cyan-300 underline-offset-8 sm:text-5xl">
              Experience
     </h2>
     <div className="max-w-3xl mx-auto text-lg leading-relaxed text-gray-300 py-8">
-    <p className="text-gray-900">Gopal Krishna Gokhale College, Kolhapur.</p>
-    <p className="text-gray-900 font-bold">2017 - 2020</p>
+    <p className="text-gray-900">Walstar Technologies Pvt.Ltd. Kolhapur.</p>
+    <p className="text-gray-900 font-bold">2020 - 2025</p>
   </div>
     </div>
-    <div className="text-center lg:col-span-2">
-    <h3 class="md:text-7xl font-bold text-black capitalize pb-8 sm:text-5xl">
-             2017 - 2022
-       </h3>
+    <div className="lg:col-span-2">
+  
+      <ul className='text-start'>
+      <li><strong>Client-Centric Solutions: </strong>Worked closely with clients to understand their business objectives and transform them into high-performing Shopify solutions, enhancing user experience and store efficiency.</li>
+      <li><strong>Responsive & Scalable Stores: </strong>Built dynamic, user-friendly Shopify stores using <strong>HTML, CSS, JavaScript, React, and Shopify Liquid,</strong> ensuring seamless performance across desktops, tablets, and mobile devices.</li>
+      <li><strong>Project Management & Delivery: </strong>Successfully managed multiple Shopify projects, consistently delivering high-quality solutions on time and within budget.</li>
+      <li><strong>Advanced Integrations & Customization: </strong>Integrated Shopify apps, third-party APIs, and custom functionalities to optimize store performance, boost conversion rates, and ensure scalability.</li>
+      <li><strong>Theme Customization & Shopify 2.0 Features: </strong>Customized Shopify themes using Online Store 2.0, dynamic sections, and meta fields to meet unique business needs.</li>
+      <li><strong>Optimized User Experience & Sales Growth: </strong>Implemented custom product filtering, advanced checkout experiences, and page speed optimization to improve user engagement and increase sales.</li>  
+      </ul>
     </div>
   </div>
 </div>
