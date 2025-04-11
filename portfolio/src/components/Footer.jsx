42947eac011e3558b2495d5c3eca5fefb73ae284
Prathamesh Surywanshi <psurywanshi620@gmail.com>
2025-04-12 00:46:06 +0530

    footer

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -5,9 +5,8 @@ const Footer = () => {
     <footer className="w-full p-4 bg-gray-900 text-white text-center">
     <p>&copy; {new Date().getFullYear()} Prathamesh | All Rights Reserved</p>
     <div className="flex justify-center gap-4 mt-2">
-      <a href="https://github.com" className="hover:text-blue-400 transition duration-300">GitHub</a>
-      <a href="https://linkedin.com" className="hover:text-blue-400 transition duration-300">LinkedIn</a>
-      <a href="https://twitter.com" className="hover:text-blue-400 transition duration-300">Twitter</a>
+      <a href="https://github.com/sp9172" className="hover:text-blue-400 transition duration-300">GitHub</a>
+      <a href="www.linkedin.com/in/prathamesh-surywanshi-03b7a7235" className="hover:text-blue-400 transition duration-300">LinkedIn</a>
     </div>
   </footer>
   )
